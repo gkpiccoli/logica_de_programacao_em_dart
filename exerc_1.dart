26b364b9f0ecf20ad0a70a9f1e4ff22264e86218
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-01 14:06:24 -0300

    exercício n1

--- exerc_1.dart
+++ exerc_1.dart
@@ -1,1 +1,18 @@
+void main(List<String> args) {
+  double peso = 82.2;
+  double altura = 1.88;
 
+  double calculaImc = peso / (altura * altura);
+  if (calculaImc >= 30) {
+    print('Precisa emagrecer amigo, você está obeso e corre risco de vida!!!!');
+  } else if (calculaImc >= 25) {
+    print('Você está em sobrepeso!!!');
+  } else if (calculaImc <= 25) {
+    print('Seu peso está normal! Paranéns!');
+  }
+  if (calculaImc <= 18.5) {
+    print('Coloque pedra nos bolsos.');
+  }
+
+  print('Seu IMC é $calculaImc');
+}
