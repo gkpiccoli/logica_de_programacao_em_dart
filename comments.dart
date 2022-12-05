c215f07f65b4e51357d6524908a021d35458a3fb
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-05 17:01:46 -0300

    comments

---
+++ comments.dart
@@ -0,0 +1,13 @@
+// comentario normal
+/* varias linhas 
+1
+2
+3
+*/
+/// dart doc
+/// Vai comentar a propriedade da variável nome.
+final String name = 'Gustavo';
+
+void main(List<String> args) {
+  print(name);
+}
