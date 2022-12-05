afc5d5788a52e194d809b5ec79142671c9574713
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-05 17:36:16 -0300

    loops

--- while.dart
+++ while.dart
@@ -1,12 +1,24 @@
-main() {
-  var contador = 0;
-  var estaRodando = true;
+import 'dart:io';
 
-  while (estaRodando) {
-    print('Hello $contador');
-    contador++;
-    if (contador >= 9) {
-      estaRodando = false;
+main() {
+  // var contador = 0;
+  // var estaRodando = true;
+
+  // while (estaRodando) {
+  //   print('Hello $contador');
+  //   contador++;
+  //   if (contador >= 9) {
+  //     estaRodando = false;
+  //   }
+  // }
+
+  var isRunning = true;
+  while (isRunning) {
+    String? valueString = stdin.readLineSync();
+    int value = int.parse(valueString!);
+    if (value > 9) {
+      isRunning = false;
     }
+    print('Rodando');
   }
 }
