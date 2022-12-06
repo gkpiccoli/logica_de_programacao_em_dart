be77ea6a68845bca9727b798bbfac7013be0ab36
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-05 22:09:15 -0300

    lista_refat

--- lista_compras.dart
+++ lista_compras.dart
@@ -8,21 +8,28 @@ main() {
   final produtos = [];
   var estaRodando = true;
   while (estaRodando) {
+    print("\x1B[2J\x1B[0;0H");
+    print(
+        '=== LISTA DE COMPRAS(${produtos.length}) - comandos [add, imprimir]');
     var input = stdin.readLineSync();
+
     if (input == 'add') {
+      print("\x1B[2J\x1B[0;0H");
       print('=== Adicione um produto ===');
       var produto = stdin.readLineSync();
       produtos.add(produto);
     } else if (input == 'imprimir') {
+      print("\x1B[2J\x1B[0;0H");
       print('=== Todos os produtos cadastrados ===');
       print('\n');
       for (var i = 0; i < produtos.length; i++) {
         print(produtos[i]);
       }
+      stdin.readLineSync();
     } else {
       estaRodando = false;
     }
   }
-
+  print("\x1B[2J\x1B[0;0H");
   print(' === FIM DO PROGRAMA ===');
 }
