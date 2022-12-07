812d6e60cc7b6b4ef90b6cdd11e5257ab0686994
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-07 11:42:23 -0300

    imc poo

--- modelando_poo.dart
+++ modelando_poo.dart
@@ -1,3 +1,24 @@
 void main() {
-  
+  final pessoa = Pessoa(
+    name: 'Gustavo',
+    altura: 1.88,
+    peso: 82,
+  );
+  print(pessoa.calculaImc());
+}
+
+class Pessoa {
+  final String name;
+  final double altura;
+  final double peso;
+
+  Pessoa({
+    required this.name,
+    required this.altura,
+    required this.peso,
+  });
+
+  double calculaImc() {
+    return peso / (altura * altura);
+  }
 }
