4d33f1fe4c167f5b3d4dc0217b9a8b5b6f133285
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-07 10:53:42 -0300

    poo final

--- poo.dart
+++ poo.dart
@@ -1,13 +1,26 @@
 main() {
-  Carro carro = Carro();
-  print(carro.nomeCompleto());
+  final lancer = Carro(
+    marca: 'Mitsubishi',
+    nome: 'Lancer Evolution',
+    portas: 5,
+  );
+  print(lancer.nomeCompleto());
+
+  final polo = Carro(marca: 'VolksWagen', nome: 'Polo Tsi', portas: 5);
+  print(polo.nomeCompleto());
 }
 
+// late é não nulo (atrasado/tardio)
 class Carro {
-  int portas = 5;
-  String nome = 'Golf';
-  String marca = 'VolksWagen';
+  late int portas;
+  late String nome;
+  late String marca;
 
+  Carro({required int portas, required String nome, required String marca}) {
+    this.marca = marca;
+    this.nome = nome;
+    this.portas = portas;
+  }
 
   // Como no exemplo abaixo, quando uma função está dentro de um classe, se transforma em uma método!!!
 
