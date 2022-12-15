2b51cfe944658e99fca127a5846dd9ba2872a068
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-15 00:05:03 -0300

    pos obr

--- curso_dois/frutas.dart
+++ curso_dois/frutas.dart
@@ -5,8 +5,18 @@ void main(List<String> args) {
   String sabor = 'Cítrica';
   int colheita = 5;
   bool isMadura = funcMadura(colheita);
+
+  print(isMadura);
+  print(funcMadura(10));//argumento se encaixa no parametro da função (int dias)
+  
+  // Posicionais Obrigatórios
+    mostrarMadura('Uva', 15);
 }
 
+
+// Função 'Madura'
+// (int dias) é um parâmetro
+// Parâmetro pertence à função
 bool funcMadura(int dias) {
   if (dias >= 30) {
     return true;
@@ -14,3 +24,16 @@ bool funcMadura(int dias) {
     return false;
   }
 }
+
+mostrarMadura(String nome, int dias) {
+  if (dias >= 30) {
+    print('A $nome está madura');
+  } else {
+    print('A $nome está verde');
+  }
+}
+
+
+// Nomeados Opcionais
+// Parâmetros "Padrão"
+// Modificador "required"
