94e02cad34ed2f113706b231af4997f8e838f489
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2022-12-14 23:24:58 -0300

    object

---
+++ curso_dois/frutas.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String nome = 'Laranja';
+  double peso = 300.2;
+  String cor = 'Verde';
+  String sabor = 'Cítrica';
+  int colheita = 5;
+  bool isMadura;
+
+
+  if (colheita >= 15 ) {
+  print('agua');
+}
+}
+
